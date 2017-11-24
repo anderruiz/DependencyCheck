3d22df6578051036caa427e71fd9a782e9dde12a
gillarramendi <gotzon@hdivsecurity.com>
2017-11-24 12:29:15 +0100

    Force old temporary files deletion

--- dependency-check-core/src/main/java/org/owasp/dependencycheck/data/update/NvdCveUpdater.java
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/data/update/NvdCveUpdater.java
@@ -50,6 +50,7 @@ import org.owasp.dependencycheck.data.update.nvd.ProcessTask;
 import org.owasp.dependencycheck.data.update.nvd.UpdateableNvdCve;
 import org.owasp.dependencycheck.utils.DateUtil;
 import org.owasp.dependencycheck.utils.Downloader;
+import org.owasp.dependencycheck.utils.FileUtils;
 import org.owasp.dependencycheck.utils.DownloadFailedException;
 import org.owasp.dependencycheck.utils.InvalidSettingException;
 import org.owasp.dependencycheck.utils.Settings;
@@ -328,6 +329,17 @@ public class NvdCveUpdater implements CachedWebDataSource {
         if (maxUpdates > 3) {
             LOGGER.info("NVD CVE requires several updates; this could take a couple of minutes.");
         }
+        
+        try {
+        		FileUtils.cleanOldTempFiles();
+        }catch (IOException e) {
+        		throw new UpdateException("Can not delete old temp files.", e);
+		}
+        try {
+	    		FileUtils.cleanOldTempFolders();
+	    }catch (IOException e) {
+	    		throw new UpdateException("Can not delete old temp dirs.", e);
+		}
 
         final Set<Future<Future<ProcessTask>>> downloadFutures = new HashSet<>(maxUpdates);
         for (NvdCveInfo cve : updateable) {
